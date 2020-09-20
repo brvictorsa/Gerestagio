268bfcee8c8c5a882c0b0adc94efc18b5d88a325
brvictorsa <victorsa.mg@gmail.com>
2020-09-20 20:19:13 -0300

    1. Criação e configuração do projeto 2. Geração do banco de dados e modelo de empresas com EF6.

---
+++ Gerestagio/Gerestagio.Domain/Entidades/EntidadeBase.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gerestagio.Domain.Entidades
+{
+    public abstract class EntidadeBase
+    {
+        public Guid Id { get; set; }
+        public DateTime CriadoEm { get; set; }
+        public DateTime AtualizadoEm { get; set; }
+    }
+}
---
+++ Gerestagio/Gerestagio/Data/ApplicationDbContext.cs
@@ -0,0 +1,28 @@
+﻿using Gerestagio.Domain.Entidades;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gerestagio.Data
+{
+    public class ApplicationDbContext: DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // PostgreSQL uses the public schema by default - not dbo.
+            modelBuilder.HasDefaultSchema("public");
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public DbSet<Empresa> Empresas { get; set; }
+    }
+}
